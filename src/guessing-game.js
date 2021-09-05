7561db739c0966d9bd99bc3b7483c64974ee78fc
nikeusov <nikotin331765115@gmail.com>
2021-09-05 21:21:42 +0300

    feat: add game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.mid = null;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.mid = Math.round((this.max - this.min) / 2) + this.min;
+  
+      return this.mid;
     }
-
+  
     lower() {
-
+      this.max = this.mid;
     }
-
+  
     greater() {
-
+      this.min = this.mid;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  module.exports = GuessingGame;
